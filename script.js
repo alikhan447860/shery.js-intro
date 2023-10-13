f8228577cd821d5e251c08a5203c09cff158ccae
alikhan447860 <112551290+alikhan447860@users.noreply.github.com>
2023-10-13 16:18:14 +0530

    with effects

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-Shery.imageEffect("#back", { style: 5, debug: true });
+Shery.imageEffect("#back", {
+  style: 5,
+  config: {
+    a: { value: 3.47, range: [0, 30] },
+    b: { value: 0.45, range: [-1, 1] },
+    aspect: { value: 2.1793214862681745 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.36, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.99, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.53, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+  gooey: true,
+});
 
 var elems = document.querySelectorAll(".elem");
 
